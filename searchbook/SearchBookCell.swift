df8a6be509163f09557884bd9922c1ad0d48e9fc
박기찬 <pkch0102@naver.com>
2018-03-04 13:02:30 +0900

    alamofire추가, 테이블 생성

---
+++ searchbook/SearchBookCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchBookCell.swift
+//  searchbook
+//
+//  Created by 박기찬 on 2018. 3. 4..
+//  Copyright © 2018년 박기찬. All rights reserved.
+//
+
+import UIKit
+
+class SearchBookCell: UITableViewCell {
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var bookInfoLabel: UILabel!
+    @IBOutlet weak var bookPriceLabel: UILabel!
+    
+}
+
+
+
--- searchbook/ViewController.swift
+++ searchbook/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchBookCell", for: indexPath) as! SearchBookCell
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
